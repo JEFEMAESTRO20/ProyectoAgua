be77cb6845e526c9e9d67d05c5a733ab4ae1bd83
Emerson Damián Lúe Gutiérrez <damianlue20@outlook.com>
2024-07-31 14:28:44 -0600

    Proyecto Agua

---
+++ src/test/java/com/ProyectoAgua/ProyectoAguaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProyectoAgua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoAguaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
